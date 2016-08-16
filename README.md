25e3357ac67d1021a3d3dffd4142a157d42c6b61
Matt Bruzek <matthew.bruzek@canonical.com>
2016-08-16 16:43:02 -0500

    Committing changes to a new repository.

---
+++ README.md
@@ -0,0 +1,43 @@
+# jenkins-workspace 
+
+This is a subordinate charm used to configure builders for a deployed 
+[jenkins charm](https://jujucharms.com/jenkins). 
+
+Jenkins is an automation engine with a plugin ecosystem that supports a variety
+of tools to create delivery pipelines, continuous integration, automated
+testing, or continuous delivery. 
+
+This subordinate customizes the workspace on any unit added to the horizontally
+scalable jenkins server.
+
+## Usage
+
+Deploy the jenkins charm and deploy the jeninks-workspace charm and relate the
+two charms in Juju:
+
+```
+juju deploy jenkins
+juju deploy jenkins-workspace
+juju add-relation jenkins jenkins-workspace
+```
+
+Then you need to add the "workspace" resource to the jenkins-workspace charm.
+The workspace resource is a compressed archive of at a minimum, a directory
+with the buildler name, and a `config.xml` with the builder definition.
+
+Add the resource using the Juju resources feature:
+
+```
+juju attach jenkins-workspace workspace=kubernetes-builder.tgz
+```
+
+# Further information
+
+This charm was built from a the 
+[jenkins-workspace layer](https://github.com/juju-solutions/layer-jenkins-workspace)
+
+Check out the [Jenkins documentation](https://jenkins.io/doc/) for more 
+information about Jenkins and workspaces.
+
+The [Jenkins charm](https://jujucharms.com/jenkins/) is available at the Juju
+Charm Store. 
---
+++ reactive/jenkins-workspace.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -ex
+
+# This bash script uses the reactive framework to configure Docker and install 
+# a Jenkins workspace. https://jujucharms.com/docs/2.0/developer-layer-example
+
+# Source the reactive framework file to get the when and when not decorators.
+source charms.reactive.sh
+
+JENKINS_WORKSPACE_DIR=/var/lib/jenkins/jobs
+
+@when_not 'jenkins-workspace.configured'
+function install_prerequisite_software() {
+  # Install the pre-requisite software needed before pulling the workspace.
+  status-set "maintenance" "Installing pre-requisite software."
+  # There are additional steps to configure before Jenkins can use Docker.
+  apt-get update -qq -y
+  # Install AUFS and the kernel extras.
+  apt-get install -y \
+    aufs-tools \
+    docker.io \
+    linux-image-extra-`uname -r` 
+  # Add the "jenkins" user to the "docker" group.
+  usermod -G docker jenkins
+  # Reconfigure the docker daemon to use AUFS instead of DeviceMapper.
+  # Append the DOCKER_OPTS to the /etc/default/docker configuration file.
+  cat << EOF > /etc/default/docker
+# THIS FILE IS MANAGED BY A BUILD SCRIPT!
+DOCKER_OPTS="--storage-driver=aufs"
+EOF
+  # Restart the docker daemon to get the new AUFS support.
+  service docker restart || true
+  # Restart jenkins so it can access the docker daemon.
+  service jenkins restart || true
+
+  charms.reactive set_state 'jenkins-workspace.configured'
+}
+
+@when_not 'jenkins-workspace.delivered'
+function deliver_resource_payload() {
+  # Use the Juju resources feature to get the workspace configuration.
+  WORKSPACE_ARCHIVE=$(resource-get workspace) || true
+  # When the archive file is zero size or workspace zip path is empty, return.
+  if [[ $? != 0 || -z "${WORKSPACE_ARCHIVE}" ]]; then
+     status-set "waiting" "Waiting for a workspace to be provided via 'juju attach'."
+     return
+  fi
+  # When the archive file exists uncompress it.
+  if [ -f "${WORKSPACE_ARCHIVE}" ]; then
+     # Uncompress the archive in the Jenkins workspace directory.
+     tar -xvzf $WORKSPACE_ARCHIVE -C $JENKINS_WORKSPACE_DIR
+     # Make sure the "jenkins" user owns the files.
+     chown -R jenkins:jenkins $JENKINS_WORKSPACE_DIR/*
+     # The configuration changed, reload Jenkins.
+     service jenkins restart || true
+     status-set "active" "Workspace uncompressed, ready to build."
+     charms.reactive set_state 'jenkins-workspace.delivered'
+  fi
+}
+reactive_handler_main
